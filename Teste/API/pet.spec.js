7035cc8102d60ad2079e0283d50feae567e64259
dautoabdula <dauto.abdula@gmail.com>
2025-12-05 11:14:20 -0300

    petspec

--- Teste/API/pet.spec.js
+++ Teste/API/pet.spec.js
@@ -6,7 +6,7 @@ const petId = 1001
 describe('API PetStore Swagger - Entidade Pet', () => {
     // Atributos, objetos, constantes e variáveis
     const request = supertest('https://petstore.swagger.io/v2') // Base URL
-
+    const massaPet = require('../../vendors/json/massaPet')
     // Funções e métodos = it
     // Incluir animal
     it('POST Pet', () => {
@@ -27,7 +27,7 @@ describe('API PetStore Swagger - Entidade Pet', () => {
     it('GET Pet', () => {
         return request
             // .get('/pet/' + petId) // tradicional
-            .get(`/pet/${petId}`)    // atual
+            .get(/pet/${petId})    // atual
             .then((response) => {
                 console.log('Resposta Recebida:\n', response.body)
                 expect(response.statusCode).toBe(200)
@@ -53,7 +53,7 @@ describe('API PetStore Swagger - Entidade Pet', () => {
 
     it('DELETE Pet', () => {
         return request
-            .delete(`/pet/${petId}`)
+            .delete(/pet/${petId})
             .then((response) => {
                 expect(response.statusCode).toEqual(200)
                 expect(response.body.code).toEqual(200)
@@ -62,5 +62,57 @@ describe('API PetStore Swagger - Entidade Pet', () => {
             })
 
     }) // Final do Delete
+
+    // POST Pet DDT - Utilizando massa de teste
+    it.each(massaPet.array.map(elemento => [
+        elemento.id,
+        elemento.categoryid,
+        elemento.categoryname,
+        elemento.petname,
+        elemento.tag0id,
+        elemento.tag0name,
+        elemento.tag1id,
+        elemento.tag1name, 
+        elemento.petstatus
+    ]))
+    ('POST Pet Data Driven: %s', (
+        id,
+        categoryid,
+        categoryname,
+        petname,
+        tag0id,
+        tag0name,
+        tag1id,
+        tag1name, 
+        petstatus
+    ) => {
+        // Modelo do json a ser enviado
+        // Valores que não forem sobrepostos seguirão como no modelo
+        const pet = require('../../vendors/json/pet.json')
+
+        // Sobreposição de valores
+        pet.id = id
+        pet.categoryid = categoryid
+        pet.categoryname = categoryname
+        pet.petname = petname
+        pet.tags[0].id = tag0id
+        pet.tags[0].name = tag0name
+        pet.tags[1].id = tag1id
+        pet.tags[1].name = tag1name
+        pet.etstatus = petstatus
+
+        // Teste em si
+        return request
+            .post('/pet')
+            .send(pet)
+            .then((response) => {
+                expect(response.statusCode).toBe(200)
+                expect(response.body.id).toBe(id)
+                expect(response.body.name).toBe(petname)
+                expect(response.body.category.name).toBe(categoryname)
+                expect(response.body.tags[0].name).toBe('vacinado')
+            })
+
+    }) // Final do Post Pet DDT
 
 })
---
+++ Vendors/CSV/massaPet.js
@@ -0,0 +1,4 @@
+id,category/id,category/name,name,tags/0/id,tags/0/name,tags/1/id,tags/1/name,status
+1001,1,dog,Snoopy,99,vacinado,100,beagle,available
+1002,1,cat,Urso,99,vacinado,100,persa,available
+1003,2,fish,Tubarao,99,vacinado,100,palhaço,available
